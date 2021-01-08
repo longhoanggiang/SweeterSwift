d03985bbb5e72b8cf55b1e6a0c802d72d451a44b
Long Hoàng Giang <longhoanggiang@gmail.com>
2021-01-08 14:47:03 +0700

    Rename hàm topViewController trùng lặp trong project, không care đủ case

--- Source/UIApplication+Sweeter.swift
+++ Source/UIApplication+Sweeter.swift
@@ -17,17 +17,17 @@ extension UIApplication {
     }
 
     /// Sweeter: Returns the currently top-most view controller.
-    public class func topViewController(base: UIViewController? = UIApplication.shared.legacyKeyWindow?.rootViewController) -> UIViewController? {
+    public class func swetter_topViewController(base: UIViewController? = UIApplication.shared.legacyKeyWindow?.rootViewController) -> UIViewController? {
         if let nav = base as? UINavigationController {
-            return topViewController(base: nav.visibleViewController)
+            return swetter_topViewController(base: nav.visibleViewController)
         }
         if let tab = base as? UITabBarController {
             if let selected = tab.selectedViewController {
-                return topViewController(base: selected)
+                return swetter_topViewController(base: selected)
             }
         }
         if let presented = base?.presentedViewController {
-            return topViewController(base: presented)
+            return swetter_topViewController(base: presented)
         }
         return base
     }
@@ -35,7 +35,7 @@ extension UIApplication {
     /// Sweeter: Show `viewController` over the top-most view controller.
     public class func present(_ viewController: UIViewController, animated: Bool = true, completion: (() -> Void)? = nil) {
         DispatchQueue.main.async {
-            topViewController()?.present(viewController, animated: animated, completion: completion)
+            swetter_topViewController()?.present(viewController, animated: animated, completion: completion)
         }
     }
 }
